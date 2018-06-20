59f589825a10f178d2ad7ab13ea541bcfd323ded
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2018-06-20 19:46:31 +0200

    fix rpm package signing test take 3

--- scripts/build/sign_packages.sh
+++ scripts/build/sign_packages.sh
@@ -6,4 +6,7 @@ gpg --allow-secret-key-import --import ~/private-repo/signing/private.key
 
 cp ./scripts/build/rpmmacros ~/.rpmmacros
 
-./scripts/build/sign_expect $GPG_KEY_PASSWORD "*.rpm"
+for package in dist/*.rpm; do
+    [ -e "$package" ] || continue
+    ./scripts/build/sign_expect $GPG_KEY_PASSWORD "dist/"$package".rpm"
+done
